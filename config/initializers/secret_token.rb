c476b2179ad6b724d1cc3d65c1ae034fbfacbb7d
jcbeaury <jcbeaury@gmail.com>
2014-05-17 03:07:56 -0400

    Initialize rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RaffleAngularjs::Application.config.secret_key_base = '6a30e3ac0c30b27c9c7d4198b795b8a6eacad2e9b8d532c3e59b8894ae74cb46ab08b98baf64e5dfef7429fd566031e973487cffc0eb5ffeb1fad9bcf3192798'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RaffleAngularjs::Application.config.session_store :cookie_store, key: '_raffle-angularjs_session'

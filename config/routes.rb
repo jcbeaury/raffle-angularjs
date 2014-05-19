239cfa581f5b9c67c6964766643706f28bfa889a
jcbeaury <jcbeaury@gmail.com>
2014-05-19 11:21:57 -0400

    Add raffle index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RaffleAngularjs::Application.routes.draw do
-  get "raffle/index"
+
+  root to: 'raffle#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

a965c42f0ccaa0595b7463bb5ba93cfd8c4c0aff
jcbeaury <jcbeaury@gmail.com>
2014-05-19 11:19:09 -0400

    Generate raffle controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RaffleAngularjs::Application.routes.draw do
+  get "raffle/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
